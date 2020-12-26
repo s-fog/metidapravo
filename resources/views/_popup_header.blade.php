7b1bb0e28b54caa630920f3bad6a67c3be9f4f08
s-fog <s-fog@yandex.ru>
2020-12-26 19:05:32 +0300

    f

--- resources/views/_popup_header.blade.php
+++ resources/views/_popup_header.blade.php
@@ -1,3 +1,4 @@
-<div class="text-white font-oranien text-32 mt-25 mb-30 mobile-550:leading-1.3 mobile-550:mb-15 mobile-550:mt-15">
+<div class="text-white font-oranien text-32 mt-25 mb-30
+mobile-550:text-28 mobile-550:leading-1.3 mobile-550:mb-15 mobile-550:mt-15">
     {!! $header !!}
 </div>
